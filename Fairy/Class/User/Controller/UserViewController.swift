3afe8661939bb8ec44c6c14a9bed7a350f100f65
丁鹏飞 <dingpf@dingpengfeideMacBook-Pro.local>
2017-06-09 19:43:23 +0800

    0

--- Fairy/Class/User/Controller/UserViewController.swift
+++ Fairy/Class/User/Controller/UserViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class UserViewController: UIViewController {
+class UserViewController: UIViewController,UITableViewDataSource {
 
+    var tableView:UITableView?
+    var dataArray:[String] = ["001","002"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         
+        tableView = UITableView(frame: view.bounds)
+        tableView?.dataSource = self
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView?.tableFooterView = UIView()
+        view.addSubview(tableView!)
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
--- Fairy/Vendor/ProgressHUD.swift
+++ Fairy/Vendor/ProgressHUD.swift
@@ -22,31 +22,26 @@ class ProgressHUD: UIView {
     private var indicatorView:UIActivityIndicatorView?
     private var textLabel:UILabel?
     
-    private var HudWidth:CGFloat = 0.0
-    private var HudHeight:CGFloat = 0.0
-    
     private var mode:Int = HudMode.Indicator.rawValue
     private var timer:Timer?
     
-    var text:String? {
+    var text:String = "" {
         didSet {
             textLabel?.text = text
             
             switch mode {
             case HudMode.CustomeView.rawValue:
-                let textS = NSString(string: (textLabel?.text)!)
-                var size = textS.boundingRect(with: CGSize(width: frame.width * 0.8, height: 0.0), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSFontAttributeName: textLabel?.font! as Any], context: nil).size
-                size = CGSize(width: max(size.height + 50.0, size.width), height: size.height)
-                
                 let indictorW = (indicatorView?.frame.width)!
-                contentView.bounds = CGRect(x: 0.0, y: 0.0, width: size.width + margin * 2.0, height: size.height + indictorW + margin * 3.0)
+                var size = sizeWithText(text: text)
+                size = CGSize(width: max(size.height + margin + indictorW, size.width), height: size.height)
+                
+                contentView.bounds.size = CGSize(width: size.width + margin * 2.0, height: size.height + indictorW + margin * 3.0)
                 indicatorView?.center = CGPoint(x: (size.width + margin * 2.0) * 0.5, y: margin + indictorW * 0.5)
                 textLabel?.frame = CGRect(origin: CGPoint(x: margin, y: indictorW + margin * 2), size: size)
                 
             case HudMode.Text.rawValue:
-                let textS = NSString(string: (textLabel?.text)!)
-                let size = textS.boundingRect(with: CGSize(width: frame.width * 0.8, height: 0.0), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSFontAttributeName: textLabel?.font! as Any], context: nil).size
-                contentView.bounds = CGRect(x: 0.0, y: 0.0, width: size.width + margin * 2.0, height: size.height + margin * 2.0)
+                let size = sizeWithText(text: text)
+                contentView.bounds.size = CGSize(width: size.width + margin * 2.0, height: size.height + margin * 2.0)
                 textLabel?.frame = CGRect(origin: CGPoint(x: margin, y: margin), size: size)
                 
             default:
@@ -70,8 +65,7 @@ class ProgressHUD: UIView {
         var hud:ProgressHUD
         if text != nil && text?.characters.count != 0 {
             hud = ProgressHUD(backView: backView!, mode: .CustomeView)
-            hud.text = text
-            
+            hud.text = text!
         } else {
             hud = ProgressHUD(backView: backView!, mode: .Indicator)
         }
@@ -128,7 +122,11 @@ class ProgressHUD: UIView {
         hud.hideHudAfterDealy(dealyTime: 1.5)
     }
     
-    //MARK:
+    //MARK: - init
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -137,10 +135,7 @@ class ProgressHUD: UIView {
     
     func initilize() {
         backgroundColor = UIColor.clear
-        HudWidth = frame.width * 0.3;
-        HudHeight = HudWidth;
         
-        contentView.frame = CGRect(x: 0.0, y: 0.0, width: HudWidth, height:HudHeight)
         contentView.center = center
         contentView.backgroundColor = KBackgroundColor
         addSubview(contentView)
@@ -170,34 +165,24 @@ class ProgressHUD: UIView {
         self.mode = mode.rawValue
         
         if mode == .Indicator {
-            HudWidth = frame.width * 0.2;
-            HudHeight = HudWidth;
-            contentView.bounds = CGRect(x: 0.0, y: 0.0, width: HudWidth, height:HudHeight)
-            indicatorView?.center = CGPoint(x: HudWidth * 0.5, y: HudHeight * 0.5)
+            textLabel?.removeFromSuperview()
+            textLabel = nil
+            
+            let hudWidth = frame.width * 0.2
+            contentView.bounds = CGRect(x: 0.0, y: 0.0, width: hudWidth, height:hudWidth)
+            indicatorView?.center = CGPoint(x: hudWidth * 0.5, y: hudWidth * 0.5)
             
         } else if mode == .CustomeView {
-            HudWidth = frame.width * 0.25;
-            HudHeight = HudWidth;
-            let indictorW = (indicatorView?.frame.width)!
-            
-            textLabel?.text = "加载中"
-            contentView.bounds = CGRect(x: 0.0, y: 0.0, width: HudWidth, height: HudHeight)
-            indicatorView?.center = CGPoint(x: HudWidth * 0.5, y: margin + indictorW * 0.5)
-            textLabel?.frame = CGRect(x: 0, y: indictorW + margin, width: HudWidth, height: HudHeight - indictorW - margin)
+            self.setValue("加载中", forKey: "text")
             
         } else if mode == .Text {
             indicatorView?.removeFromSuperview()
             indicatorView = nil
-            
-            HudWidth = frame.width * 0.25;
-            HudHeight = HudWidth * 0.6;
-            
-            textLabel?.text = "加载中"
-            contentView.bounds = CGRect(x: 0.0, y: 0.0, width: HudWidth, height: HudHeight)
-            textLabel?.frame = CGRect(x: margin, y: margin, width: HudWidth - margin * 2.0, height: HudHeight - margin * 2.0)
+            self.setValue("加载中", forKey: "text")
         }
     }
     
+    //MARK:
     private func hideHudAfterDealy(dealyTime:TimeInterval) {
         timer = Timer.scheduledTimer(timeInterval: dealyTime, target: self, selector: #selector(hideAnimated), userInfo: nil, repeats: false)
         RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
@@ -205,14 +190,21 @@ class ProgressHUD: UIView {
     
     @objc private func hideAnimated() {
         UIView.animate(withDuration: 0.3, animations: {
-            self.contentView.transform = CGAffineTransform.init(scaleX: 0.1, y: 0.1)
+            self.contentView.transform = CGAffineTransform.init(scaleX: 0.5, y: 0.5)
         }) { (flag) in
             self.timer?.invalidate()
+            self.timer = nil
             self.removeFromSuperview()
         }
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    //MARK:
+    private func sizeWithText(text:String) -> CGSize {
+        let textS = NSString(string: (textLabel?.text)!)
+        let size = textS.boundingRect(with: CGSize(width: frame.width * 0.8, height: 0.0),
+                                      options: NSStringDrawingOptions.usesLineFragmentOrigin,
+                                      attributes: [NSFontAttributeName: textLabel?.font! as Any],
+                                      context: nil).size
+        return size
     }
 }
